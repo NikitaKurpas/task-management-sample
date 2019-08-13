de11411988ad7e2abc866bc4076a9b0de6cbea05
Nikita Kurpas <nikita@etheric.agency>
2019-08-13 21:30:49 +0200

    Add styled-components and jwt decoder, add decoded jwt payload to Auth

--- frontend/components/Auth/Auth.tsx
+++ frontend/components/Auth/Auth.tsx
@@ -1,51 +1,76 @@
-import React, { createContext, useContext, useMemo, useState } from "react";
-import { IUser } from "../../../common/types/common";
-import { useApiRead } from '../API/APIReadRequest'
-import Router from 'next/router'
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useMemo,
+  useState
+} from "react";
+import { IJwtTokenPayload, IUser } from "../../../common/types/common";
+import { useApiRead } from "../API/APIReadRequest";
+import Router from "next/router";
+import jwtDecode from 'jwt-decode';
 
 type Auth = {
-  loading: boolean
+  loading: boolean;
   user: IUser | null;
   token: string | null;
-  initAuth: (token: string) => any
-  logout: () => any
+  tokenUser: IJwtTokenPayload | null;
+  initAuth: (token: string) => any;
+  logout: () => any;
 };
 
 const AuthContext = createContext<Auth>({
   loading: false,
   user: null,
   token: null,
+  tokenUser: null,
   initAuth: () => {},
-  logout: () => {},
+  logout: () => {}
 });
 
 export const AuthProvider: React.FunctionComponent = ({ children }) => {
-  // const token = localStorage.getItem('_t')
-  const [token, setToken] = useState<string | null>(localStorage.getItem("_t"))
-  const [user, setUser] = useState<IUser | null>(null)
-  const { data, loading, error, refetch } = useApiRead<IUser>('/users/me', { fetchProtectedOnly: true })
+  const [token, setToken] = useState<string | null>(null);
+  const [tokenUser, setTokenUser] = useState<IJwtTokenPayload | null>(null);
+  const [user, setUser] = useState<IUser | null>(null);
+  const { data, loading, error, refetch } = useApiRead<IUser>("/users/me", {
+    fetchProtectedOnly: true
+  });
+
+  useEffect(() => {
+    const token = localStorage.getItem("_t")
+    if (!token) return
+    setToken(token)
+    setTokenUser(jwtDecode(token))
+  }, []); // Get token in browser only
 
   if (!user && !loading && !error && data) {
-    setUser(data)
+    setUser(data);
   }
 
-  const value = useMemo<Auth>(() => ({
-    loading,
-    user,
-    token,
-    initAuth: async (token: string) => {
-      localStorage.setItem("_t", token);
-      setToken(token)
-      setUser(null)
-      await refetch()
-    },
-    logout: async () => {
-      localStorage.removeItem("_t");
-      setToken(null)
-      setUser(null)
-      await Router.push('/login')
-    }
-  }), [loading, user, token]);
+  const value = useMemo<Auth>(
+    () => ({
+      loading,
+      user,
+      token,
+      tokenUser: tokenUser,
+      initAuth: async (token: string) => {
+        localStorage.setItem("_t", token);
+        setToken(token)
+        setTokenUser(jwtDecode(token))
+        setUser(null);
+        await refetch();
+      },
+      logout: async () => {
+        localStorage.removeItem("_t");
+        setToken(null);
+        setTokenUser(null)
+        setUser(null);
+        await Router.push("/login");
+      }
+    }),
+    [loading, user, token]
+  );
+
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
